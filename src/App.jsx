531547e7939d5634ce4ea51b1d7fcd2c8ac95ce1
imran688-dev <farabiahmedimran543@gmail.com>
2024-05-10 09:49:41 +0600

    Some modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { useEffect } from 'react'
 import './App.css'
+import { useState } from 'react';
+
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:1337/api/blogs?populate=*").then((res) => res.json())
+      .then((data) => {
+        console.log("strapi blogs:", data.data);
+        setBlogs(data.data);
+        // console.log(`http://localhost:1337${data.data[0].attributes.Image.data[0].attributes.url}`);
+        // console.log(data.data[0].attributes.Title);
+      });
+  }, []);
 
   return (
     <>
-      <h1 className='text-4xl text-red-500 text-center font-semibold mt-5'>Hello Imran</h1>
+      <section className='max-w-[700px] p-20 mx-auto'>
+        {
+          blogs.map((blog) => (
+            <div key={blog.id} className='shadow mb-5 p-10 text-center'>
+              <h2 className='mb-3 text-red-500 font-semibold text-2xl'>{blog.attributes.Title}</h2>
+              < img src={`http://localhost:1337${blog.attributes.Image.data[0].attributes.url}`} alt="" />
+              <p className='mt-6 text-justify'>{blog.attributes.Description[0].children[0].text}</p>
+            </div>
+          ))
+        }
+      </section >
     </>
   )
 }
