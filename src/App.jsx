c604cb3390049393059789f2fa2562cd8dbd894a
imran688-dev <farabiahmedimran543@gmail.com>
2024-06-04 20:39:47 +0600

    new header design

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,30 @@
-// import { useEffect } from 'react'
 import './App.css'
-// import { useState } from 'react';
+import Header from './components/Header/Header'
+// import { Route, Routes } from 'react-router-dom'
+// import Home from './Pages/Home'
+// import About from './Pages/About'
+// import Contact from './Pages/Contact'
+// import Service from './Pages/Service'
+// import Blog from './Pages/Blog'
+// import Notfound from './Pages/Notfound'
+// import Footer from './components/Footer/Footer'
 
 
 function App() {
-  // const [blogs, setBlogs] = useState([]);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:1337/api/blogs?populate=*").then((res) => res.json())
-  //     .then((data) => {
-  //       console.log("strapi blogs:", data.data);
-  //       setBlogs(data.data);
-  //       // console.log(`http://localhost:1337${data.data[0].attributes.Image.data[0].attributes.url}`);
-  //       // console.log(data.data[0].attributes.Title);
-  //     });
-  // }, []);
-
-
 
 
   return (
     <>
-
-
-
-
-
-
-
-
-      {/* <section className='max-w-[700px] p-20 mx-auto'>
-        {
-          blogs.map((blog) => (
-            <div key={blog.id} className='shadow mb-5 p-10 text-center'>
-              <h2 className='mb-3 text-red-500 font-semibold text-2xl'>{blog.attributes.Title}</h2>
-              < img src={`http://localhost:1337${blog.attributes.Image.data[0].attributes.url}`} alt="" />
-              <p className='mt-6 text-justify'>{blog.attributes.Description[0].children[0].text}</p>
-            </div>
-          ))
-        }
-      </section> */}
+      <Header />
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/*" element={<Notfound />} />
+      </Routes>
+      <Footer /> */}
     </>
   )
 }
